d6757d642042fc059a59834eb181d34499f1304f
lkw <523830372@qq.com>
2020-12-22 11:08:23 +0800

    init repository

---
+++ test/java/util/ArrayListTest.java
@@ -0,0 +1,12 @@
+package test.java.util;
+
+import java.util.ArrayList;
+
+public class ArrayListTest {
+
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(1);
+        System.out.println(list);
+    }
+}
